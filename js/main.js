55780832f81349694228310e7a21b2df4c895980
Lene4ka <lenya.gusev.99@mail.ru>
2022-10-04 22:04:00 +0500

    Проверка длины + Рандомные числа

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function randomInteger(min, max) {
+  if (max < min || min < 0 || max == min)
+  {
+    return "Неправильно задан диапазон";
+  }
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
+
+function checkLength(str,max)
+{
+ let len = str.length;
+ if (len <= max) return true;
+ return false;
+}
+
