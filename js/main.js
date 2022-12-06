6efd4c5e03b32e95952d126e5724b26ae77619d0
Lene4ka <lenya.gusev.99@mail.ru>
2022-12-06 15:13:58 +0500

    BigPictures

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
-function randomInt(min, max) {
-  if (max < min || min < 0 || max == min)
-  {
-    return "Неправильно задан диапазон";
-  }
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
-  return Math.round(rand);
-}
+import { addPhoto } from "./data.js";
+import './util.js';
 
-function checkLength(str,max)
-{
- let len = str.length;
- if (len <= max) return true;
- return false;
-}
 
+addPhoto();
