9305cf9ce41ccc21384b8954626d4618f839398b
Lene4ka <71835365+Lene4ka@users.noreply.github.com>
2022-12-07 18:43:48 +0500

    Update pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -1,4 +1,5 @@
 import { objectsArray } from './data';
+import { openBigPicture} from './big-picture.js'
 
 const picturesBlock = document.querySelector('.pictures');
 const pictureTemp = document.querySelector('#picture').content.querySelector('.picture');
@@ -10,7 +11,11 @@ const renderPicture = (picture) => {
   element.src = picture.url;
   element.querySelector('.picture__likes').textContent = picture.likes;
   element.querySelector('.picture__comments').textContent = picture.comments.length;
+  element.addEventListener('click',(evt) => {
+    evt.preventDefault();
 
+    openBigPicture(picture);
+  });
   return element;
 };
 
