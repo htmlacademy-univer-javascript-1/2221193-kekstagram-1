e04f3bd9d941a3d8999740b31fe69f2cab104e05
Lene4ka <71835365+Lene4ka@users.noreply.github.com>
2022-12-07 16:36:38 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,8 +1,7 @@
 import { getRandomInt } from './get-random-number.js';
 
-const getRandomElement = (array) => {
-  return array[getRandomInt(0, array.length - 1)];
-};
+const getRandomElement = (array) =>  array[getRandomInt(0, array.length - 1)];
+
 
 const isEscape = (evt) => evt.key === 'Escape';
 
