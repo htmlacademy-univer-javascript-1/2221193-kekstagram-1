4c0e81bc62ba43600782e51f8e9b4db00749d57e
Lene4ka <lenya.gusev.99@mail.ru>
2022-12-12 16:09:22 +0500

    module11-task1

---
+++ js/fetch.js
@@ -0,0 +1,23 @@
+const Url = {
+  GET: 'https://26.javascript.pages.academy/kekstagram/data',
+  POST: 'https://26.javascript.pages.academy/kekstagram',
+};
+
+const sendRequest = (onSuccess, onFail, method, body) => {
+  fetch(
+    Url[method],
+    {
+      method: method,
+      body: body,
+    },
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      onSuccess(data);
+    })
+    .catch(() => {
+      onFail();
+    });
+};
+
+export { sendRequest };
